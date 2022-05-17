4a00555b9bc563260204c63d84ffd58ef6c94478
youjin Cho <ud071233@gmail.com>
2022-05-17 15:26:30 +0900

    Feat: 회원가입 form 검사 코드 작성

---
+++ frontend/assets/js/login.js
@@ -0,0 +1,61 @@
+// 로그인/회원가입 전환
+$(".change-btn").on("click", function(e) {
+  e.preventDefault();
+  const id = "#" + $(this).attr("id").split("-")[0];
+  const chagned_id = id === "#login" ? "#signup" : "#login";
+
+  $(chagned_id).hide();
+  $(id).fadeIn(600);
+});
+
+// 이메일/비밀번호 중복 체크
+$(".check").on("click", function(e) {
+  e.preventDefault();
+
+  const id = "#" + $(this).prev().attr("id");
+  const helper = `${id}-helper-text`;
+
+  // 입력 여부 확인
+  if ($(id).val() === undefined || $(id).val() === "") {
+    alert("값을 입력하세요.");
+    return;
+  }
+
+  if (id === "#email") { // 이메일 중복체크인 경우
+    const email = $(id).val();
+
+    if (!(/$^|.+@.+..+/.test(email))) {
+      $(helper).text("잘못된 이메일 형식입니다.");
+    } else $(helper).text("");
+
+    // 이메일 중복 검사 시행
+
+  } else if (id === "#nickname") { // 닉네임 중복체크인 경우
+    // 닉네임 중복 검사 시행
+    $(helper).text("닉네임검사");
+  }
+});
+
+// 비밀번호 체크
+$("#pwd1").on("propertychange change keyup paste input", function() {
+  const id = "#" + $(this).attr("id");
+  const helper = `${id}-helper-text`;
+  const pwd = $(this).val();
+
+  if (pwd.length < 8)
+    $(helper).text("비밀번호는 최소 8자입니다.");
+  else if (pwd.length > 15)
+    $(helper).text("비밀번호는 8~15자로 구성되어야 합니다.");
+  else
+    $(helper).text(/^(?=.*[a-zA-Z])((?=.*\d)+(?=.*\W)).{8,15}$/.test(pwd) ? "" : "영문자, 숫자, 특수문자를 혼용해야 합니다.");
+});
+
+$("#pwd2").on("propertychange change keyup paste input", function() {
+  const id = "#" + $(this).attr("id");
+  const helper = `${id}-helper-text`;
+  const pwd1 = $("#pwd1").val();
+  const pwd2 = $(this).val();
+  if (pwd1 !== pwd2)
+    $(helper).text("비밀번호가 같지 않습니다.");
+  else $(helper).text("");
+});
