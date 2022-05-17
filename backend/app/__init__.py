18774b07074b589f7b948c8a822b0765e55fd797
youjin Cho <ud071233@gmail.com>
2022-05-17 21:04:29 +0900

    Feat: 모델 생성

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from config.flask_config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     # 앱 설정
     app = Flask(__name__)
     app.config.from_object((get_flask_env()))
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from model import models
 
     return app
 
