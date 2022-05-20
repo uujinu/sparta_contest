c0c88384f5cd768c30f73c33e84d7744ca4a2bee
youjin Cho <ud071233@gmail.com>
2022-05-20 23:05:44 +0900

    Fix: recipe의 likes를 숫자로 출력되도록 수정

--- backend/util/dto.py
+++ backend/util/dto.py
@@ -1,28 +1,5 @@
-from flask_restx import Namespace, fields, Model
-
-
-user_base = Model('user_base', {
-    'id': fields.Integer(required=True, description='회원 식별자'),
-    'email': fields.String(required=True, description='회원 이메일'),
-    'nickname': fields.String(required=True, description='회원 닉네임'),
-    'profile_image': fields.String(required=False, description='회원 프로필 이미지')
-})
-
-recipe_base = Model('recipe_base', {
-    'id': fields.Integer(required=True, description='레시피 식별자'),
-    'author': fields.Nested(user_base, required=True, description='레시피 작성자', attribute='user'),
-    'title': fields.String(required=True, description='레시피 제목'),
-    'thumbnail': fields.String(required=False, description='레시피 썸네일 이미지'),
-    'likes': fields.Integer(requierd=False, description='레시피 좋아요 수')
-})
-
-
-class ImageGet(fields.Raw):
-    def format(self, value):
-        res = []
-        for img in value:
-            res.append(img.img_path)
-        return res
+from flask_restx import Namespace, fields
+from .dto_base import *
 
 
 class UserDto:
