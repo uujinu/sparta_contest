3de7e1b668dd841c1f477bfcb67a066a561b68dd
youjin Cho <ud071233@gmail.com>
2022-05-21 07:27:07 +0900

    Feat: 에러핸들러 생성 및 등록

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,6 +5,7 @@ from flask_restx import Api
 from flask_wtf.csrf import CSRFProtect
 from router.recipes import api as Recipe_ns
 from router.users import api as User_ns
+from util.errors.error_handling import app_error_handler, api_error_handler
 
 
 blueprint = Blueprint('api', __name__)
@@ -26,6 +27,11 @@ def create_app():
               title='쩝쩝박사 Project API',
               version='1.0',
               description='JJBS project flask server')
+
+    # 에러 핸들러 등록
+    app_error_handler(app)
+    api_error_handler(User_ns)
+    api_error_handler(Recipe_ns)
 
     api.add_namespace(User_ns, path='/users')
     api.add_namespace(Recipe_ns, path='/recipes')
