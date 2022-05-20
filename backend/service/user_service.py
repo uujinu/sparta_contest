a5dc7ea05c9f4a746848fbf4cf8bce805a88ac47
youjin Cho <ud071233@gmail.com>
2022-05-21 07:55:14 +0900

    Feat: 로그인데코레이터 추가, 오류 발생 시 abort 응답

--- backend/service/user_service.py
+++ backend/service/user_service.py
@@ -1,7 +1,8 @@
 from app.extension import db, login_manager
 from model.models import User, generate_password_hash
 from .user_form import *
-from flask_login import current_user, login_user
+from flask_restx import abort
+from flask_login import current_user, login_user, login_required
 
 
 # 회원 생성
@@ -26,20 +27,13 @@ def save_new_user(data):
             }
             return response_object, 201
     else:
-        response_object = {
-            'status': 'fail',
-            'message': '이미 존재하는 회원입니다. 로그인해주세요.',
-        }
-        return response_object, 409
+        abort(409, '이미 존재하는 회원입니다. 로그인해주세요.')
 
 
 # 회원 로그인
 def sign_in_user(data):
     if current_user.is_authenticated:
-        return {
-            'status': 'fault',
-            'message': '이미 로그인된 사용자입니다.'
-        }
+        abort(400, '이미 로그인된 사용자입니다.')
 
     user = User.query.filter_by(email=data['email']).first()
     if user:
@@ -48,30 +42,25 @@ def sign_in_user(data):
             # check the password
             if user.check_password(password=form.password.data):  # 로그인 성공
                 login_user(user)
-                return {
+                response_object = {
                     'status': 'login success',
                     'message': '로그인에 성공하였습니다.'
                 }
-
-        else:  # 로그인 실패
-            return {
-                'status': 'login failed',
-                'message': '로그인에 실패하였습니다.'
-            }
-
+                return response_object, 200
+            else:  # 로그인 실패
+                abort(401, '로그인에 실패하였습니다.')
     else:
-        return {
-            'status': 'login failed',
-            'message': '등록되지 않은 사용자입니다. 회원가입하세요.'
-        }
+        abort(401, '등록되지 않은 사용자입니다. 회원가입하세요.')
 
 
 # 모든 회원 get
+@login_required
 def get_all_users():
     return User.query.all()
 
 
 # 특정 회원 get
+@login_required
 @login_manager.user_loader
 def get_a_user(id):
     return User.query.filter_by(id=id).first()
@@ -84,6 +73,7 @@ def save_changes(data):
 
 
 # 데이터 삭제
+@login_required
 def delete_data(data):
     db.session.delete(data)
     db.session.commit()
