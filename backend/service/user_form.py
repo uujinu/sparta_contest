510e73b4b54aa7542d88f69e2c426aaf849296eb
youjin Cho <ud071233@gmail.com>
2022-05-22 02:26:12 +0900

    Feat: 비밀번호 변경을 위한 form 생성

--- backend/service/user_form.py
+++ backend/service/user_form.py
@@ -3,16 +3,17 @@ from wtforms import StringField, EmailField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo
 
 
-class RegisterForm(FlaskForm):
-    nickname = StringField('nickname', validators=[DataRequired()])
-    email = EmailField('email', validators=[DataRequired()])
-    password = PasswordField('password', validators=[
-                             DataRequired(), EqualTo('password2')])
-    password2 = PasswordField('password', validators=[DataRequired()])
-    submit = SubmitField('submit')
-
-
 class LoginForm(FlaskForm):
     email = EmailField('email', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField('submit')
+
+
+class PasswordManageForm(LoginForm):
+    password = PasswordField('password', validators=[
+                             DataRequired(), EqualTo('password2')])
+    password2 = PasswordField('password', validators=[DataRequired()])
+
+
+class RegisterForm(PasswordManageForm):
+    nickname = StringField('nickname', validators=[DataRequired()])
