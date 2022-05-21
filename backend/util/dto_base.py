71cbcfbee86cc5090786a0bdec4a54d1d17fcd3c
youjin Cho <ud071233@gmail.com>
2022-05-21 15:24:51 +0900

    Fix: required parameter 수정

--- backend/util/dto_base.py
+++ backend/util/dto_base.py
@@ -22,8 +22,8 @@ user_base = Model('user_base', {
 })
 
 recipe_base = Model('recipe_base', {
-    'id': fields.Integer(required=True, description='레시피 식별자'),
-    'author': fields.Nested(user_base, required=True, description='레시피 작성자', attribute='user'),
+    'id': fields.Integer(required=False, description='레시피 식별자'),
+    'author': fields.Nested(user_base, required=False, description='레시피 작성자', attribute='user'),
     'title': fields.String(required=True, description='레시피 제목'),
     'thumbnail': fields.String(required=False, description='레시피 썸네일 이미지'),
     'likes': LikesNumGet(attribute='like_users', required=False, description='레시피 좋아요 수')
