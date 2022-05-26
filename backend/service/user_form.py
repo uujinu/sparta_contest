d8a70a5302a85678d082b3538104eea1559e3f22
youjin Cho <ud071233@gmail.com>
2022-05-27 01:52:49 +0900

    Feat: 이메일, 비밀번호 validator 추가

--- backend/service/user_form.py
+++ backend/service/user_form.py
@@ -1,11 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, Regexp
+
+
+email_regex = '^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$'
+email_validator = Regexp(email_regex, message='잘못된 이메일 형식입니다.')
+
+passwd_regex = '^(?=.*[a-zA-Z])((?=.*\d)+(?=.*\W)).{8,15}$'
+passwd_validator = Regexp(passwd_regex, message='잘못된 비밀번호 형식입니다.')
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('email', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    email = EmailField('email', validators=[DataRequired(), email_validator])
+    password = PasswordField('password', validators=[
+                             DataRequired(), passwd_validator])
     submit = SubmitField('submit')
 
 
