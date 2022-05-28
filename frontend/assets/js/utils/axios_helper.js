e2f59ed271f3aac43c85053b44336833f4fd5677
youjin Cho <ud071233@gmail.com>
2022-05-28 23:20:23 +0900

    Feat: 세션 만료시 로그아웃 후 재로그인 요청

--- frontend/assets/js/utils/axios_helper.js
+++ frontend/assets/js/utils/axios_helper.js
@@ -1,3 +1,6 @@
+import { current_user } from "../user/user_profile.js";
+
+
 export function axiosWrapper(
   method,
   url,
@@ -12,6 +15,14 @@ export function axiosWrapper(
     async (error) => {
       const status = error.response? error.response.status : null;
       console.log("status: ", status);
+      if (status === 401) {
+        const user = current_user();
+        if (user) {
+          alert("세션이 만료되었습니다. 다시 로그인 해주세요.");
+          localStorage.removeItem("user");
+          location.replace("/login");
+        }
+      }
       return Promise.reject(error);
     }
   );
