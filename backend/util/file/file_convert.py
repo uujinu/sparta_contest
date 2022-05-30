8b37ebdf1db3b0888d9bb1d00560c47a65ea04fb
youjin Cho <ud071233@gmail.com>
2022-05-31 02:11:14 +0900

    Fix: s3 이미지 컨버팅 함수 오류 수정

--- backend/util/file/file_convert.py
+++ backend/util/file/file_convert.py
@@ -2,14 +2,14 @@ from PIL import Image
 from io import BytesIO
 
 
-def image_to_bytes(self, img):
+def image_to_bytes(img):
     res = BytesIO()  # 파일 객체(BytesIO)로 컨버팅
     img.save(res, format='JPEG')
     res.seek(0)  # 바이트 스트림의 offset을 0으로 변경
     return res
 
 
-def convert_image(self, img, w, h):
+def convert_image(img, w, h):
     _t = Image.open(img).copy()
     temp = _t.convert('RGB')
     temp.thumbnail((w, h), Image.ANTIALIAS)
