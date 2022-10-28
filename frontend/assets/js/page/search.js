87796721a2432f3938e35bd8cb82e725df2f6820
youjin Cho <ud071233@gmail.com>
2022-10-28 22:47:59 +0900

    Feat: 검색페이지 pagination 기능 추가

--- frontend/assets/js/page/search.js
+++ frontend/assets/js/page/search.js
@@ -1,5 +1,6 @@
 import { axiosWrapper } from "../utils/axios_helper.js";
 import { throttle, mouse_event, search_func, auto_list_select, auto_list_ctrl } from "../search.js"
+import { init_nav } from "../utils/pagination.js";
 
 
 // 검색어 자동완성 선택(클릭)
@@ -71,48 +72,114 @@ function MainSearch() {
 };
 
 
+// 레시피 붙이는 함수
+function recipe_pagination(post_ul, start, end, data) {
+  const default_img = "https://jjbs-s3.s3.ap-northeast-2.amazonaws.com/static/profile_basic.png";
+  const default_thumb = "https://jjbs-s3.s3.ap-northeast-2.amazonaws.com/static/thumb_basic.png";
+  const len = data.length;
+
+  for (let i = start; i < len && i < end; i++) {
+    const temp_html = `<li class="post-list-li">
+                        <div class="post-card-box">
+                          <a href="/recipe/${data[i].id}" class="card-link">
+                            <img src="${data[i].thumbnail ? data[i].thumbnail : default_thumb}" class="post-img">
+                          </a>
+                        </div>
+                        <div class="post-card-info">
+                          <div class="card-info-tit">${data[i].title}</div>
+                          <div class="card-info-btm">
+                            <div class="card-info-user">
+                              <a href="/profile/${data[i].author.id}">
+                                <img src="${data[i].author.profile_image ? data[i].author.profile_image : default_img}">
+                                ${data[i].author.nickname}
+                              </a>
+                            </div>
+                            <div class="card-btm">
+                              <span><i class="bi bi-heart-fill"></i> 좋아요 ${data[i].likes}</span>
+                            </div>
+                          </div>
+                        </div>
+                      </li>`;
+    post_ul.append(temp_html);
+  }
+};
+
+
+// nav 버튼 클릭 이벤트
+function nav_event(page_num, post_ul, data) {
+  const nav_ul = $(".pagination");
+  const nav_li = nav_ul.find("a");
+  const idx = 30;
+
+  nav_li.on("click", function(e) {
+    post_ul.empty();
+    const _label = $(this).attr("aria-label"); 
+    const _page = parseInt($(this).attr("href").substring(1)); 
+    const start = (_page - 1) * idx;
+    const end = start + idx;
+
+    if (_label === "Previous" || _label === "Next") {
+      init_nav(nav_ul, page_num, _page);
+      recipe_pagination(post_ul, start, end, data)
+    } else {
+      recipe_pagination(post_ul, start, end, data);
+    }
+  });
+};
+
+
+function recipe_init(post_ul, data, page_num) {
+  const nav_ul = $(".pagination");
+  const nav_li = nav_ul.find("a");
+  const idx = 30;
+
+  nav_li.on("click", function(e) {
+    post_ul.empty();
+    const _label = $(this).attr("aria-label"); 
+    const _page = parseInt($(this).attr("href").substring(1)); 
+    const start = (_page - 1) * idx;
+    const end = start + idx;
+
+    if (_label === "Previous" || _label === "Next") {
+      init_nav(nav_ul, page_num, _page);
+      recipe_pagination(post_ul, start, end, data);
+      nav_event(page_num, post_ul, data);
+    } else {
+      recipe_pagination(post_ul, start, end, data);
+    }
+  });
+};
+
+
 // 검색 결과
 function SearchResult() {
   const query_str = new URLSearchParams(location.search);
-  const default_img = "https://jjbs-s3.s3.ap-northeast-2.amazonaws.com/static/profile_basic.png";
-  const default_thumb = "https://jjbs-s3.s3.ap-northeast-2.amazonaws.com/static/thumb_basic.png";
   const search_span = $(".search-kw");
   const post_ul = search_span.next().children(":first");
+  const idx = 30;
+  const nav_ul = $(".pagination");
 
   if (query_str.get("q")) {
     search_span.text(`'${query_str.get("q")}'에 대한 검색 결과입니다.`);
+    const _page = location.hash ? parseInt(location.hash.substring(1)) : 1;
 
     let _url = `/recipes/search?q=${encodeURIComponent(query_str.get("q"))}`;
     if (query_str.get("id")) {
       _url += `&id=${query_str.get("id")}`;
     }
     axiosWrapper("GET", _url, null, (res) => {
+      const _page_num = (res.data.length % idx) ? parseInt(res.data.length / idx) + 1 : parseInt(res.data.length / idx);
       post_ul.empty();
+
+      init_nav(nav_ul, _page_num);
+      nav_ul.on("click", function() {
+        recipe_init(post_ul, res.data, _page_num);
+      });
+
       if (res.data.length) {
-        for (let i = 0; i < res.data.length; i++) {
-          const temp_html = `<li class="post-list-li">
-                              <div class="post-card-box">
-                                <a href="/recipe/${res.data[i].id}" class="card-link">
-                                  <img src="${res.data[i].thumbnail ? res.data[i].thumbnail : default_thumb}" class="post-img">
-                                </a>
-                              </div>
-                              <div class="post-card-info">
-                                <div class="card-info-tit">${res.data[i].title}</div>
-                                <div class="card-info-btm">
-                                  <div class="card-info-user">
-                                    <a href="/profile/${res.data[i].author.id}">
-                                      <img src="${res.data[i].author.profile_image ? res.data[i].author.profile_image : default_img}">
-                                      ${res.data[i].author.nickname}
-                                    </a>
-                                  </div>
-                                  <div class="card-btm">
-                                    <span><i class="bi bi-heart-fill"></i> 좋아요 ${res.data[i].likes}</span>
-                                  </div>
-                                </div>
-                              </div>
-                            </li>`;
-          post_ul.append(temp_html);
-        }
+        const start = (_page - 1) * idx;
+        const end = start + idx;
+        recipe_pagination(post_ul, start, end, res.data);
       } else {
         post_ul.append(`<p>검색 결과가 없습니다.</p>`);
       }
